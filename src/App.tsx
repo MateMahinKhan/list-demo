13c645319955733f5c7204ae3b3edf0ca6915ad0
Muhammad Mahin Khan <tutenstine@gmail.com>
2023-03-21 20:42:18 -0400

    fixed test

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learxned React
+          Learn React
         </a>
       </header>
     </div>
